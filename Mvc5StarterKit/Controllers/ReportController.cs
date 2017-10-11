d99b3738b8e4da5e1d3bfae9f6f8b1d00c680eca
Binh Tran <binhtran@kms-technology.com>
2017-10-11 16:44:00 +0700

    Create Advanced Report Parts page

--- Mvc5StarterKit/Controllers/ReportController.cs
+++ Mvc5StarterKit/Controllers/ReportController.cs
@@ -25,5 +25,10 @@ namespace Mvc5StarterKit.Controllers
         {
             return View();
         }
+
+        public ActionResult AdvancedReportParts()
+        {
+            return View();
+        }
     }
 }
